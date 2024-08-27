4812acf284347c937b2730ba78705baf3f4a580f
pascualmanuel <manuelnpascual@gmail.com>
2024-08-27 16:10:51 -0300

    Plugin Check Warnings solved

--- react-bridge/react-bridge.php
+++ react-bridge/react-bridge.php
@@ -7,7 +7,6 @@ Author: Labba Studio - Manuel
 Author URI: https://www.labba.studio/
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-
 */
 
 // Añadir el menú de React Bridge WP en el panel de administración
@@ -30,8 +29,8 @@ function react_plugin_settings_page() {
     <div class="wrap">
         <h1>Configuración del Plugin React</h1>
         <form method="post" enctype="multipart/form-data" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
-        <input type="hidden" name="action" value="handle_file_upload" />
-        <?php wp_nonce_field('react_plugin_file_upload', 'react_plugin_file_upload_nonce'); ?>
+            <input type="hidden" name="action" value="handle_file_upload" />
+            <?php wp_nonce_field('react_plugin_file_upload', 'react_plugin_file_upload_nonce'); ?>
 
             <table class="form-table">
                 <tr valign="top">
@@ -64,9 +63,14 @@ function handle_file_upload() {
         wp_die('No tienes permisos suficientes para acceder a esta página.');
     }
 
-    // Verificar el nonce para mayor seguridad
     if (!isset($_POST['react_plugin_file_upload_nonce']) || !wp_verify_nonce($_POST['react_plugin_file_upload_nonce'], 'react_plugin_file_upload')) {
         wp_die('Error de seguridad. Acción no permitida.');
+    }
+
+    global $wp_filesystem;
+    if (empty($wp_filesystem)) {
+        require_once ABSPATH . 'wp-admin/includes/file.php';
+        WP_Filesystem();
     }
 
     if (isset($_FILES['react_plugin_build_upload']) && !empty($_FILES['react_plugin_build_upload']['tmp_name'])) {
@@ -75,28 +79,23 @@ function handle_file_upload() {
         $build_path = $plugin_dir . 'build/';
         $temp_extract_path = $plugin_dir . 'temp_extract/';
 
-        if (!file_exists($build_path)) {
-            wp_mkdir_p($build_path);
+        if (!$wp_filesystem->exists($build_path)) {
+            $wp_filesystem->mkdir($build_path);
         }
-        if (!file_exists($temp_extract_path)) {
-            wp_mkdir_p($temp_extract_path);
+        if (!$wp_filesystem->exists($temp_extract_path)) {
+            $wp_filesystem->mkdir($temp_extract_path);
         }
 
         $zip = new ZipArchive;
         if ($zip->open($uploaded_file['tmp_name']) === TRUE) {
             // Limpia el directorio build si ya existe
-            $files = glob($build_path . '*');
+            $files = $wp_filesystem->dirlist($build_path, true);
             foreach ($files as $file) {
-                if (is_file($file)) {
-                    unlink($file);
-                } elseif (is_dir($file)) {
-                    $dir = new RecursiveDirectoryIterator($file, FilesystemIterator::SKIP_DOTS);
-                    $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::CHILD_FIRST);
-                    foreach ($files as $fileinfo) {
-                        $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
-                        $todo($fileinfo->getRealPath());
-                    }
-                    rmdir($file);
+                $file_path = $build_path . $file['name'];
+                if ($file['type'] === 'f') {
+                    $wp_filesystem->delete($file_path);
+                } elseif ($file['type'] === 'd') {
+                    $wp_filesystem->rmdir($file_path, true);
                 }
             }
 
@@ -106,40 +105,37 @@ function handle_file_upload() {
 
             // Verifica si hay una carpeta adicional "build" dentro de la extracción
             $extracted_build_path = $temp_extract_path . 'build/';
-            if (file_exists($extracted_build_path)) {
-                $files = glob($extracted_build_path . '*');
+            if ($wp_filesystem->exists($extracted_build_path)) {
+                $files = $wp_filesystem->dirlist($extracted_build_path, true);
                 foreach ($files as $file) {
-                    $dest = $build_path . basename($file);
-                    rename($file, $dest);
+                    $file_path = $extracted_build_path . $file['name'];
+                    $dest = $build_path . $file['name'];
+                    $wp_filesystem->move($file_path, $dest);
                 }
 
                 // Elimina la carpeta adicional
-                rmdir($extracted_build_path);
+                $wp_filesystem->rmdir($extracted_build_path, true);
             } else {
                 // Si no hay una carpeta adicional, mueve el contenido directamente
-                $files = glob($temp_extract_path . '*');
+                $files = $wp_filesystem->dirlist($temp_extract_path, true);
                 foreach ($files as $file) {
-                    $dest = $build_path . basename($file);
-                    rename($file, $dest);
+                    $file_path = $temp_extract_path . $file['name'];
+                    $dest = $build_path . $file['name'];
+                    $wp_filesystem->move($file_path, $dest);
                 }
             }
 
             // Limpia el directorio temporal
-            $temp_files = glob($temp_extract_path . '*');
+            $temp_files = $wp_filesystem->dirlist($temp_extract_path, true);
             foreach ($temp_files as $file) {
-                if (is_file($file)) {
-                    unlink($file);
-                } elseif (is_dir($file)) {
-                    $dir = new RecursiveDirectoryIterator($file, FilesystemIterator::SKIP_DOTS);
-                    $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::CHILD_FIRST);
-                    foreach ($files as $fileinfo) {
-                        $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
-                        $todo($fileinfo->getRealPath());
-                    }
-                    rmdir($file);
+                $file_path = $temp_extract_path . $file['name'];
+                if ($file['type'] === 'f') {
+                    $wp_filesystem->delete($file_path);
+                } elseif ($file['type'] === 'd') {
+                    $wp_filesystem->rmdir($file_path, true);
                 }
             }
-            rmdir($temp_extract_path);
+            $wp_filesystem->rmdir($temp_extract_path, true);
 
             wp_redirect(add_query_arg('upload_status', 'success', wp_get_referer()));
             exit;
@@ -152,20 +148,19 @@ function handle_file_upload() {
         exit;
     }
 }
-
 add_action('admin_post_handle_file_upload', 'handle_file_upload');
 add_action('admin_post_nopriv_handle_file_upload', 'handle_file_upload');
 
 // Encolar los scripts de React y ReactDOM
 function enqueue_react_scripts() {
-    // Encolar React y ReactDOM desde un CDN
-    wp_enqueue_script('react-script', 'https://unpkg.com/react/umd/react.development.js', array(), null, true);
-    wp_enqueue_script('react-dom-script', 'https://unpkg.com/react-dom/umd/react-dom.development.js', array('react-script'), null, true);
+    // Enqueue React and ReactDOM from a CDN with versioning
+    wp_enqueue_script('react-script', 'https://unpkg.com/react/umd/react.development.js', array(), '18.2.0', true); // Added version
+    wp_enqueue_script('react-dom-script', 'https://unpkg.com/react-dom/umd/react-dom.development.js', array('react-script'), '18.2.0', true); // Added version
 
     $plugin_dir = plugin_dir_path(__FILE__) . 'build/';
 
     if (!file_exists($plugin_dir)) {
-        return; // Si no existe la carpeta build, no hacer nada
+        return; // If the build folder doesn't exist, do nothing
     }
 
     $js_files = glob($plugin_dir . 'static/js/*.js');
@@ -174,7 +169,7 @@ function enqueue_react_scripts() {
     $js_file = null;
     $css_file = null;
 
-    // Filtra solo los archivos que comienzan con 'main.'
+    // Filter only files starting with 'main.'
     foreach ($js_files as $file) {
         if (strpos(basename($file), 'main.') === 0 && pathinfo($file, PATHINFO_EXTENSION) === 'js') {
             $js_file = basename($file);
@@ -190,14 +185,15 @@ function enqueue_react_scripts() {
     }
 
     if ($js_file) {
-        wp_enqueue_script('my-react-app-js', plugins_url('build/static/js/' . $js_file, __FILE__), array('react-script', 'react-dom-script'), null, true);
+        wp_enqueue_script('my-react-app-js', plugins_url('build/static/js/' . $js_file, __FILE__), array('react-script', 'react-dom-script'), '1.0.0', true); // Added version
     }
 
     if ($css_file) {
-        wp_enqueue_style('my-react-app-css', plugins_url('build/static/css/' . $css_file, __FILE__));
+        wp_enqueue_style('my-react-app-css', plugins_url('build/static/css/' . $css_file, __FILE__), array(), '1.0.0'); // Added version
     }
 }
 add_action('wp_enqueue_scripts', 'enqueue_react_scripts');
+
 
 function add_react_container() {
     if (is_page_template('template-react.php')) {
@@ -208,11 +204,18 @@ add_action('wp_footer', 'add_react_container');
 
 // Crear y activar el tema temporal
 function activate_temporary_theme() {
+    global $wp_filesystem;
+    if (empty($wp_filesystem)) {
+        require_once ABSPATH . 'wp-admin/includes/file.php';
+        WP_Filesystem();
+    }
+
+
     $theme_dir = WP_CONTENT_DIR . '/themes/react-bridge-theme';
 
-    if (!file_exists($theme_dir)) {
+    if (!$wp_filesystem->exists($theme_dir)) {
         // Crear el directorio del tema
-        wp_mkdir_p($theme_dir);
+        $wp_filesystem->mkdir($theme_dir);
 
         // Crear archivos básicos del tema
         $theme_files = [
@@ -243,7 +246,7 @@ Author URI: https://www.labba.studio/
         ];
 
         foreach ($theme_files as $file => $content) {
-            file_put_contents($theme_dir . '/' . $file, $content);
+            $wp_filesystem->put_contents($theme_dir . '/' . $file, $content);
         }
     }
 
@@ -254,7 +257,6 @@ Author URI: https://www.labba.studio/
     // Crear página con el template
     create_react_page();
 }
-
 add_action('plugins_loaded', 'activate_temporary_theme');
 
 // Crear una página personalizada con un template específico
@@ -263,7 +265,13 @@ function create_react_page() {
     $page_content = '[react_app]';
     $page_template = 'template-react.php';
 
-    if (!get_page_by_title($page_title)) {
+    $page = new WP_Query(array(
+        'post_type' => 'page',
+        'title' => $page_title,
+        'posts_per_page' => 1
+    ));
+
+    if (!$page->have_posts()) {
         $page_id = wp_insert_post(array(
             'post_title' => $page_title,
             'post_content' => $page_content,
@@ -274,5 +282,4 @@ function create_react_page() {
         update_post_meta($page_id, '_wp_page_template', $page_template);
     }
 }
-
 ?>
